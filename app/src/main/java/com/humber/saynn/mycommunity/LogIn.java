c3a5b3db40058cfe1dac6f54ccfd26b232c788e3
Sinan <syaman14@ku.edu.tr>
2019-12-08 22:44:20 -0500

    Log in and Sign up Done

--- app/src/main/java/com/humber/saynn/mycommunity/LogIn.java
+++ app/src/main/java/com/humber/saynn/mycommunity/LogIn.java
@@ -1,35 +1,94 @@
 package com.humber.saynn.mycommunity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogIn extends AppCompatActivity {
 
     Button signUpButton;
-    EditText emailEditText;
+    EditText emailEditText,passwordEditText;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        emailEditText = findViewById(R.id.emailEditText);
+        firebaseAuth = FirebaseAuth.getInstance();
+        emailEditText = findViewById(R.id.emailEditTextLogin);
+        passwordEditText = findViewById(R.id.passwordEditTextLogin);
         signUpButton = findViewById(R.id.signUpButton);
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),SignUp.class);
+                Intent i = new Intent(getApplicationContext(), SignUp.class);
                 String tempEmail = emailEditText.getText().toString();
-                if(!emailEditText.getText().toString().equals("")){
-                    i.putExtra("email",emailEditText.getText().toString());
+                if (!emailEditText.getText().toString().equals("")) {
+                    i.putExtra("email", emailEditText.getText().toString());
                 }
                 startActivity(i);
             }
         });
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            final FirebaseUser user = firebaseAuth.getCurrentUser();
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (user != null) {
+                    Intent i = new Intent(getApplicationContext(), ChooseCommunity.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please Login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+    }
+
+    public void signIn(View v){
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        if(email.isEmpty()){
+            emailEditText.setError("Please enter email");
+            emailEditText.requestFocus();
+        }else if(password.isEmpty()){
+            passwordEditText.setError("Please enter password");
+            passwordEditText.requestFocus();
+        }else{
+            firebaseAuth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(LogIn.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(),"Login can't be completed.",Toast.LENGTH_SHORT).show();
+                            }else{
+                                Intent i = new Intent(getApplicationContext(), ChooseCommunity.class);
+                                startActivity(i);
+                            }
+                        }
+                    });
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
     }
 }
--- app/src/main/java/com/humber/saynn/mycommunity/SignUp.java
+++ app/src/main/java/com/humber/saynn/mycommunity/SignUp.java
@@ -1,5 +1,6 @@
 package com.humber.saynn.mycommunity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,30 +8,73 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
 
-    EditText emailEditText;
+    EditText emailEditText, nameEditText, surnameEditText, passwordEditText, usernameEditText;
     Button signUpButton;
+    FirebaseAuth mFirebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
         emailEditText = findViewById(R.id.emailEditText);
+        nameEditText = findViewById(R.id.nameEditText);
+        surnameEditText = findViewById(R.id.surnameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        usernameEditText = findViewById(R.id.usernameEditText);
         signUpButton = findViewById(R.id.signUpButton);
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),ChooseCommunity.class);
-                startActivity(i);
-            }
-        });
+
         Intent intent = getIntent();
         if (intent.hasExtra("email")) {
             String emailEntered = intent.getStringExtra("email");
             emailEditText.setText(emailEntered);
         }
 
+    }
+
+    public void registerAccount(View v){
+        String email = emailEditText.getText().toString();
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String name = nameEditText.getText().toString();
+        String surname = surnameEditText.getText().toString();
+
+        if(email.isEmpty()){
+            emailEditText.setError("Please enter email");
+            emailEditText.requestFocus();
+        }else if(username.isEmpty()){
+            usernameEditText.setError("Please enter username");
+            usernameEditText.requestFocus();
+        }else if(password.isEmpty()){
+            passwordEditText.setError("Please enter password");
+            passwordEditText.requestFocus();
+        }else if(name.isEmpty() || surname.isEmpty()){
+            Toast.makeText(getApplicationContext(),"Fields are empty.",Toast.LENGTH_SHORT).show();
+            nameEditText.requestFocus();
+        }else {
+            mFirebaseAuth.createUserWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(),task.getException()+"",Toast.LENGTH_SHORT).show();
+                            }else{
+                                Intent i = new Intent(getApplicationContext(),ChooseCommunity.class);
+                                startActivity(i);
+                            }
+                        }
+                    });
+        }
     }
 }
