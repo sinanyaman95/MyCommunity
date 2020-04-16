4467c5589dfb74028c1399234d0248d8c3a30e83
Sinan <syaman14@ku.edu.tr>
2020-04-16 18:09:43 +0300

    Added foods to db

--- app/src/main/java/com/humber/saynn/mycommunity/entities/FoodContent.java
+++ app/src/main/java/com/humber/saynn/mycommunity/entities/FoodContent.java
@@ -27,8 +27,13 @@ public class FoodContent {
 
         String url = "https://www.google.com/maps/place/Churrasqueira+Sao+Miguel/@43.7195661,-79.5353751,15z/data=!4m8!1m2!2m1!1srestaurant!3m4!1s0x0:0xfd0d031a63c03b66!8m2!3d43.7207223!4d-79.5369896";
         tempFoodList.add(new Food("Churrasqueira", R.drawable.friedchicken,url));
+        tempFoodList.add(new Food("Rogan Josh",R.drawable.rogan_josh,"https://goo.gl/maps/45Vh9QDFjPNmP1Xd9","indian"));
         tempFoodList.add(new Food("Burger Boy", R.drawable.burgersandwich,"https://www.google.com/maps/place/Burger+Boy/@43.7298735,-79.6043775,15z/data=!4m8!1m2!2m1!1srestaurant!3m4!1s0x0:0xea0062a6e6d11251!8m2!3d43.7311737!4d-79.6006599"));
         tempFoodList.add(new Food("Pizza Nova", R.drawable.pizza,"https://www.google.com/maps/place/Pizza+Nova/@43.7298735,-79.6043775,15z/data=!4m8!1m2!2m1!1srestaurant!3m4!1s0x0:0x6c53e25056d75224!8m2!3d43.7331816!4d-79.5885417"));
+        tempFoodList.add(new Food("Lentil Soup", R.drawable.mercimek,"https://g.page/Lezizkitchen?share","turkish"));
+        tempFoodList.add(new Food("Kebab", R.drawable.kebap,"https://goo.gl/maps/NeYhecJ5YRGuNn7S6","turkish"));
+        tempFoodList.add(new Food("Manti", R.drawable.manti,"https://goo.gl/maps/Pnd6kvCGk17nDFbu7","turkish"));
+        tempFoodList.add(new Food("Garam Masala", R.drawable.garam_masala,"https://goo.gl/maps/4dNxbk7FDPSLtdmn9","indian"));
         tempFoodList.add(new Food("Al-Aruba", R.drawable.cheese,"https://www.google.com/maps/place/Al-Aruba+Restaurant/@43.7307089,-79.5482199,15z/data=!4m8!1m2!2m1!1srestaurant!3m4!1s0x0:0xaa4eec105e102482!8m2!3d43.7254675!4d-79.5514253","indian"));
         tempFoodList.add(new Food("Da Nang", R.drawable.popcorn,"https://www.google.com/maps/place/Da+Nang+Restaurant/@43.7295251,-79.5198383,14.75z/data=!4m8!1m2!2m1!1srestaurant!3m4!1s0x0:0x75bc4b1b3ff7f89e!8m2!3d43.7409799!4d-79.5130777"));
         tempFoodList.add(new Food("Eggsmart", R.drawable.friedegg,"https://www.google.com/maps/place/Eggsmart/@43.695627,-79.5296338,14.5z/data=!4m8!1m2!2m1!1srestaurant!3m4!1s0x0:0xf2e9e730ce964a3!8m2!3d43.702269!4d-79.5231038"));
@@ -37,6 +42,7 @@ public class FoodContent {
         tempFoodList.add(new Food("Mc Donalds", R.drawable.burgersandwich,"https://www.google.com/maps/place/McDonald%27s/@43.668974,-79.3436474,15z/data=!4m9!1m3!2m2!1srestaurant!6e5!3m4!1s0x0:0xef9f4c49265c1145!8m2!3d43.6764621!4d-79.3585423"));
         tempFoodList.add(new Food("Pilav",R.drawable.pilavpng,"https://goo.gl/maps/2t657H25UwwqWHxT9","Turkish"));
         tempFoodList.add(new Food("Irish Pub", R.drawable.beer,url));
+        tempFoodList.add(new Food("Pongal",R.drawable.pongal,"https://goo.gl/maps/11BXEimDUa4rFWnW7","indian"));
         tempFoodList.add(new Food("Biscuit", R.drawable.biscuit,url));
         tempFoodList.add(new Food("Beverage", R.drawable.cocacola,url));
         tempFoodList.add(new Food("Beverage", R.drawable.pepsi,url));
